0628159748051d8ca3f3541130d8854ab1696ae3
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-12 17:25:39 +0900

    Python

--- Python/파이썬실습문제/문제02.py
+++ Python/파이썬실습문제/문제02.py
@@ -3,3 +3,12 @@ l = 0
 for i in word:
     l += 1
     print(l)
+
+word = 'happy!'
+count = 0
+# 모든 문자를 돌면서
+for char in word:
+# 1씩 증가시킨다.
+    count = count + 1
+    # count += 1
+    print(count)
