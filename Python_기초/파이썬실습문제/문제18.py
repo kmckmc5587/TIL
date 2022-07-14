45abde2a3240d74a378d116e4a2234fe55f34746
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-14 17:52:23 +0900

    Python

--- Python_기초/파이썬실습문제/문제18.py
+++ Python_기초/파이썬실습문제/문제18.py
@@ -8,3 +8,16 @@ for chr in string:
     else:
         dic[chr] = 1
 print(dic)
+
+word = 'banana'
+
+result = {}
+for char in word:
+    # 딕셔너리에 키가 있으면?
+    if not char in result:
+        # 키랑 값을 0으로 초기화한다.
+        result[char] = 1
+    # 딕셔너리에 키가 없으면?
+    else:
+        result[char] = result[char] + 1
+        print(result)
