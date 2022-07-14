2428e97f07ddf7268f64f0bb27da9914105b87ae
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-14 11:24:02 +0900

    Python

---
+++ Python_기초/practice/4일차/return.py
@@ -0,0 +1,20 @@
+# a라는 이름에 타입이 int, 1을 할당
+a = 1
+
+print(1 + 2)
+print(3 * 3)
+print(4 * 5)
+
+# 한 단계 up!
+a = [1, 2, 3]
+# sum(a)의 결과(int, 합의 계산 결과)를 result 할당
+result = len(a)
+
+numbers = [1, 3, 2]
+# None이 반환되고, numbers는 바뀌어있고
+result = numbers.reverse()
+# 그래서 일반적으로 코드는
+numbers.reverse()
+
+
+print('1 2 3'.split().index('2') + 10)
