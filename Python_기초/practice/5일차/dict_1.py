82221fd5cdd157daa8c27a30b4b6b5aa7800ae67
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-18 10:00:31 +0900

    Python

---
+++ Python_기초/practice/5일차/dict_1.py
@@ -0,0 +1,11 @@
+word = 'banana'
+result = {}
+for char in word:
+    # result[char] = # 없으면 KeyError
+    # result.get(char, 0) # 없으면 None, 기본값을 주면 0
+    result[char] = result.get(char, 0) + 1
+
+print(result)
+# 키-값의 추가
+#result['a'] = 0
+# print(result)
