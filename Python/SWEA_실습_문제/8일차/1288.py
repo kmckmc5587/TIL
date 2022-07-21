bf922aef97e7f092157d376c7b41fe6b97e77720
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-21 16:10:29 +0900

    Python

--- Python/SWEA_실습_문제/8일차/1288.py
+++ Python/SWEA_실습_문제/8일차/1288.py
@@ -2,3 +2,20 @@
 
 # sys.stdin = open("1288_input.txt", "r")
 
+T = int(input())
+for t in range(1, T + 1):
+    n = input()
+    a = [0 for _ in range(10)]
+    intn = int(n)
+    count = 1
+
+    while 0 in a:
+        for i in n:
+            if a[int(i)] > 0:
+                continue
+            else:
+                a[int(i)] += 1
+        count += 1
+        n = str(intn * count)
+    count -= 1
+    print('# %d %d' % (t, intn * count) )
