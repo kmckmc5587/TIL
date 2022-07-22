cfa7d5679a7d7199f86c731f54a2141881042310
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-22 11:40:58 +0900

    Python

--- Python/프로젝트_02/04_tmdb.py
+++ Python/프로젝트_02/04_tmdb.py
@@ -4,6 +4,7 @@
 import requests
 BASE_URL = 'https://api.themoviedb.org/3'
 path = '/movie/1414'
+# api_key 발급받아서 각자 키를 넣어주세요.
 params = {
     'api_key': '8854669b886a6c07c12ea947bcc2311d',
     'language': 'ko-KR'
