1b1db7db19001f98cd7aed33fdd745a3a189445a
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-29 10:21:58 +0900

    Python

---
+++ Python/BAEKJOON/4일차/1764.py
@@ -0,0 +1,17 @@
+# 이름을 key로 사용해서 저장할 딕셔너리 변수
+dict_ = dict()
+dict_['ohhenrie'] = '듣도못한사람'
+dict_['charlie'] = '듣도못한사람'
+dict_['baesangwook'] = '듣도못한사람'
+
+if 'obama' in dict_:
+    print('obama 듣도보도못한사람')
+
+if 'baesangwook' in dict_:
+    print('baesangwook 듣도보도못한사람')
+
+if 'ohhenrie' in dict_:
+    print('ohhenrie 듣도보도못한사람')
+
+if 'clinton' in dict_:
+    print('clinton 듣도보도못한사람')
