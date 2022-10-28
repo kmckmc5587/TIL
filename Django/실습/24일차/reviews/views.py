f0497d5a1053b4596fbc1b6b44a1c2a91c7ec87c
kmckmc5587 <kmckmc5587@gmail.com>
2022-10-28 17:47:08 +0900

    Django

---
+++ Django/실습/24일차/reviews/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
+
+# Create your views here.
+
+def index(request):
+    reviews = Review.objects.order_by('-pk')
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'reviews/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:index')
+    else:
+        review_form = ReviewForm()
+    context = {
+        'review_form': review_form
+    }
+    return render(request, 'reviews/new.html', context=context)
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        'review': review
+    }
+    return render(request, 'reviews/detail.html', context)
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:detail', review.pk)
+    else: 
+        review_form = ReviewForm(instance=review)
+    context = {
+        'reviewform': review_form
+    }
+    return render(request, 'reviews/update.html', context)
+
+def delete(request, pk):
+    Review.objects.get(pk=pk).delete()
+    return redirect('reviews:index')
